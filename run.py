c2ab6312550beadd9057cde894186728d44f1c29
Matt Manning <mtmanning93@hotmail.co.uk>
2023-03-21 13:34:18 +0000

    Add function to search by song title and priint information based on the song

--- run.py
+++ run.py
@@ -37,8 +37,8 @@ def select_search_type():
             f"\nYou selected to search via {choice}) {search_menu.get(choice)}...\n")
         if choice == 'A':
             search_by_artist_name()
-        # elif choice == 'B':
-        #     search_by_song_title()
+        elif choice == 'B':
+            search_by_song_title()
         # elif choice == 'C':
         #     search_by_genre()
         # elif choice == 'D':
@@ -60,6 +60,7 @@ def search_by_artist_name():
     column_1 = SHEET.worksheet('library').col_values(1)
     artist_list = column_1[1:]
 
+    #Try except possibility
     if artist_name in artist_list:
         appearances = SHEET.worksheet('library').findall(artist_name)
         # print row information of all instances of the artists name.
@@ -70,6 +71,32 @@ def search_by_artist_name():
         print("""Sorry, we couldnt find that artist in our library. Please try 
         again.\n""")
         search_by_artist_name()
+
+    return artists_song_list
+
+
+def search_by_song_title():
+    """
+    Enables user to search via the song title.
+    Displays list of all entries in the library.
+    """
+    song_title = input("Enter song title: ").lower()
+    print(f"\nYou searched for {song_title}. Searching...\n")
+
+    column_2 = SHEET.worksheet('library').col_values(2)
+    song_title_list = column_2[1:]
+
+    #Try except possibility
+    if song_title in song_title_list:
+        title = SHEET.worksheet('library').find(song_title)
+        song_title_info = SHEET.worksheet('library').row_values(title.row)
+        print(song_title_info)
+    else:
+        print("""Sorry, we couldnt find that song in our library. Please try 
+        again.\n""")
+        search_by_song_title()
+
+    return song_title_info
 
 
 select_search_type()
